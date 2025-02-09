225fd35739bf3b7c53b446e42c3c27f43f25f568
cashely <290119516@qq.com>
2025-02-09 15:42:44 +0000

    feat: 调试微信小程序登录 #dev

--- packages/api/src/utils/wechat.sdk.js
+++ packages/api/src/utils/wechat.sdk.js
@@ -4,23 +4,32 @@ import loginRouter from "../routes/login";
 
 
 function WechatSDK(options) {
+    console.log(options)
     this.appid = options.appid;
-    this.appsecret = options.appsecret;
+    this.secret = options.secret;
     this.prefix = 'https://api.weixin.qq.com';
 }
 
 WechatSDK.prototype.get = function (...args) {
-    return $.get(args);
+    const [url, ...otherArgs] = args;
+    return $.get(`${this.prefix}${url}`, ...otherArgs);
 }
 
 WechatSDK.prototype.code2Session = async function (code) {
     const res = await this.get('/sns/jscode2session', {
-        appid: this.appid,
-        secret: this.secret,
-        js_code: code,
-        grant_type: 'authorization_code'
+        params: {
+            appid: this.appid,
+            secret: this.secret,
+            js_code: code,
+            grant_type: 'authorization_code'
+        }
     });
-    console.log(res, '<------------code2Session的返回')
+    if (res.status === 200 && !res.data.errcode) {
+        return res.data;
+    } else {
+        throw new Error(res.data.errmsg);
+    }
+    
 }
 
 const wechatSdk = new WechatSDK(wxConfig);
