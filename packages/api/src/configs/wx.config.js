78dcccfcdeb6b2a9eed9cc5ee09496b523ad8184
cashely <290119516@qq.com>
2025-03-26 22:09:07 +0800

    fix: 修改预约模版通知 #dev

--- packages/api/src/configs/wx.config.js
+++ packages/api/src/configs/wx.config.js
@@ -13,7 +13,7 @@ export default {
     private_key: fs.readFileSync('cert/1710304781/apiclient_key.pem').toString(), // 微信商户私钥
     serial_no: '2841C23DCDE8B701726E199976BB75764937ED49',  // 微信商户证书序列号
     apiv3_private_key: '2MEA8rnt9pg2mea8rnt9pg2mea8rnt99', // 微信商户apiv3私钥
-    notify_url: 'https://api.verlantum.cn/wx/notify_url', // 微信支付回调地址
+    notify_url: 'https://api.verlantum.cn/wx/wxpay/notify_url', // 微信支付回调地址
     payTemplateId: 'fIijh96IYidJFYVTWwW2FsvEu2b7yKaQ7MO9FDv8M7U',
     refundTemplateId: 'fIijh96IYidJFYVTWwW2FsvEu2b7yKaQ7MO9FDv8M7U',
 }
--- packages/api/src/routes/wx.router.js
+++ packages/api/src/routes/wx.router.js
@@ -1,6 +1,8 @@
+import payment from '../utils/wechat.pay.sdk';
 import Router from "../middles/route";
 import prisma from "../configs/prisma";
-import payment from "../utils/wechat.pay.sdk";
+import { sendTemplateMessage } from "../utils/wechat.helper";
+import wxConfig from '../configs/wx.config';
 
 const router = new Router();
 
@@ -159,6 +161,123 @@ router.post('/refund_notify_url', async (req, res) => {
             message: error.message,
             code: 'FAIL'
         }) 
+    }
+})
+
+router.post('/wxpay/notify_url', async (req, res) => {
+    try {
+        const { event_type } = req.body;
+        if (event_type === "TRANSACTION.SUCCESS") {
+            // 支付成功
+            // 1. 解密resource
+            // 2. 更新订单状态
+            // 3. 发送模板消息
+            // 4. 发送微信通知
+
+            // 1. 解密resource
+            const { resource } = req.body;
+            const payResult = await payment.decodeResource(resource);
+            console.log('收到微信支付通知', payResult)
+            // 2. 更新订单状态
+            const { out_trade_no } = payResult;
+            const updateBook = await prisma.book.update({
+                where: {
+                    outTradeNo: out_trade_no
+                },
+                data: {
+                    status: 1 // 已预约
+                },
+                include: {
+                    user: true
+                }
+            });
+
+            const { wxid } = updateBook.user;
+
+            // 3. 发送模板消息
+            const templateId = wxConfig.payTemplateId;
+            await sendTemplateMessage({
+                templateId,
+                openid: wxid,
+                data: {
+                    // 商品
+                    thing1: {
+                        value: '预约成功'
+                    },
+                    // 金额
+                    amount2: {
+                        value: '预约时间'
+                    },
+                    // 时间
+                    date3: {
+                        value: '预约人'
+                    },
+                    // 订单编号
+                    character_string8: {
+                        value: '预约人电话'
+                    }
+                }
+            })
+
+        } else if (event_type === 'REFUND.SUCCESS') {
+            // 退款成功
+            // 1. 解密resource
+            // 2. 更新订单状态
+            // 3. 发送模板消息
+            // 4. 发送微信通知
+
+            // 1. 解密resource
+            const { resource } = req.body;
+            const refundResult = await payment.decodeResource(resource);
+            console.log('收到微信退款通知', refundResult)
+            const { out_refund_no } = refundResult;
+
+            // 2. 更新订单状态
+            const updateBook = await prisma.book.update({
+                where: {
+                    outRefundNo: out_refund_no
+                },
+                data: {
+                    status: 4 // 已退款
+                },
+                include: {
+                    user: true
+                }
+            });
+            // 3. 发送模板消息
+            const { wxid } = updateBook.user;
+            const templateId = wxConfig.refundTemplateId;
+            await sendTemplateMessage({
+                templateId,
+                openid: wxid,
+                data: {
+                    // 退款状态
+                    phrase1: {
+                        value: '预约成功'
+                    },
+                    // 订单编号
+                    character_string2: {
+                        value: '预约时间'
+                    },
+                    // 商品名称
+                    thing3: {
+                        value: '预约人'
+                    },
+                    // 退款金额
+                    amount4: {
+                        value: '预约人电话'
+                    }
+                }
+            })
+
+            // 4. 发送微信通知
+
+        } else {
+            throw new Error("接受微信支付失败");
+        }
+        req.response.success("接受微信支付/退款回调成功", event_type);
+    } catch (error) {
+        res.response.error(error);
     }
 })
 
