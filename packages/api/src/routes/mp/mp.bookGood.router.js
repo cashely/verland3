cca3b9473225a65a41abf3a3150deb778e12f870
cashely <290119516@qq.com>
2025-03-28 00:27:44 +0800

    fix: 修复take错误 #dev

--- packages/api/src/routes/mp/mp.bookGood.router.js
+++ packages/api/src/routes/mp/mp.bookGood.router.js
@@ -8,7 +8,7 @@ const router = new Router({
 
 router.get('/', async (req, res) => {
     try {
-        const { pageSize, pageNo } = req.query;
+        const { pageSize = 20, pageNo = 1 } = req.query;
         const bookGoods = await prisma.bookGood.findMany({
             include: {
                 thumb: true
