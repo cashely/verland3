5098decb052b8b4df594a65bd45804f738761c7c
cashely <290119516@qq.com>
2025-03-29 00:24:00 +0800

    fix: 修复订单没有菜单的问题 #dev

--- packages/api/src/routes/wx.router.js
+++ packages/api/src/routes/wx.router.js
@@ -89,7 +89,7 @@ router.post('/wxpay/notify_url', async (req, res) => {
             const payResult = await payment.decodeResource(resource);
             console.log('收到微信支付通知', payResult)
             // 2. 更新订单状态
-            const { out_trade_no, amount: { payer_total, transaction_id: transactionId } } = payResult;
+            const { out_trade_no, amount: { payer_total }, transaction_id: transactionId } = payResult;
 
             const book = await prisma.book.findFirst({
                 where: {
