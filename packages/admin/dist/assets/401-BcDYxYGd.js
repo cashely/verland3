ec439e41613ab970f83d6189597b165c9bc15764
chenhaihu <hyyrz_888@163.com>
2025-03-30 12:48:15 +0800

    feat: dist #dev

--- packages/admin/dist/assets/401-BcDYxYGd.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BuFq4bDe.js";const o=()=>r.jsxs("div",{children:[r.jsx("h1",{children:"401 - Unauthorized"}),r.jsx("p",{children:"Sorry, you are not authorized to view this page."})]});export{o as default};
