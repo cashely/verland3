b18ed577518739290438e364029b6762ca8336c1
chenhaihu <hyyrz_888@163.com>
2025-03-30 12:46:36 +0800

    feat: dist #dev

--- packages/admin/dist/assets/index-CrEBwHKv.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CxPArkTe.js";const e=()=>r.jsx("div",{children:"32"});export{e as default};
---
+++ packages/admin/dist/assets/index-DnCAJ_OH.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BuFq4bDe.js";const e=()=>r.jsx("div",{children:"32"});export{e as default};
