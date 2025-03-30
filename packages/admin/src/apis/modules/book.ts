c17830a93324fd0155cba28284e12b0005ddb455
chenhaihu <hyyrz_888@163.com>
2025-03-30 23:33:35 +0800

    feat: 完善功能 #dev

--- packages/admin/src/apis/modules/book.ts
+++ packages/admin/src/apis/modules/book.ts
@@ -30,7 +30,8 @@ export async function edit<T>(
 /**
  * @name 查询退款详情
  */
-export async function refundDetail<T>(outTradeNo: string): Promise<RequestResult<T>> {
-  return request.get(`/wxpay/refundStatu/${outTradeNo}`); 
+export async function refundDetail<T>(
+  outTradeNo: string
+): Promise<RequestResult<T>> {
+  return request.get(`/wxpay/refundStatu/${outTradeNo}`);
 }
-
