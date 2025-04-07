4b7aed701cb20118289a71f27ee741be85c7e544
cashely <290119516@qq.com>
2025-04-07 21:44:02 +0800

    fix: 修复接口字段必填校验 #dev

--- packages/api/src/routes/mp/mp.book.router.js
+++ packages/api/src/routes/mp/mp.book.router.js
@@ -15,7 +15,6 @@ router.post('/', validate(z => (
             area: z.string().min(1),
             detail: z.string().min(1),
             petname: z.string().min(1),
-            weight: z.number().int().min(1),
             age: z.number().min(1),
             type: z.string().min(1),
             subType: z.string().min(1),
