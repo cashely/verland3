f066dcdd8773f40eb7c170157b15a3ca9cf85a9c
cashely <290119516@qq.com>
2025-04-08 21:53:49 +0800

    fix: 修复预约地址字段传递错误 #dev

---
+++ packages/api/prisma/migrations/20250408135221_2025040801/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "bookDateTime" DROP NOT NULL;
--- packages/api/src/routes/mp/mp.book.router.js
+++ packages/api/src/routes/mp/mp.book.router.js
@@ -21,7 +21,7 @@ router.post('/', validate(z => (
             subType: z.string().min(1),
             bookGoodIds: z.array(z.string()).optional(),
             menuId: z.string().min(1),
-            bookDateTime: z.string().min(1),
+            bookDateTime: z.string().optional(),
             handleWay: z.number().int().default(1),
             handleDateTime: z.string().optional(),
             isRite: z.union([z.literal(1), z.literal(2)]).default(2),
