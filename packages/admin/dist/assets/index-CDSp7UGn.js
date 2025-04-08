c0ade0cb93a3e7eccc8cf1fdc29eac8d636f7dcf
cashely <290119516@qq.com>
2025-04-08 22:56:14 +0800

    fix: 修复预约地址字段传递错误 #dev

---
+++ packages/admin/dist/assets/index-CDSp7UGn.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CKOsE6kc.js";const s=()=>t.jsx("div",{});export{s as default};
--- packages/admin/dist/assets/index-dr_cAH04.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Ch_8Y76k.js";const s=()=>t.jsx("div",{});export{s as default};
