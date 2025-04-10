39d30ee817fffc190eba16e3d267e80084e9ce65
cashely <290119516@qq.com>
2025-04-10 23:13:18 +0800

    fix: 修复修改接口 #dev

---
+++ packages/admin/dist/assets/index-BQSjLovf.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BZ4MVbkq.js";const s=()=>t.jsx("div",{});export{s as default};
--- packages/admin/dist/assets/index-Bv953RqI.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DDS0J3Vo.js";const s=()=>t.jsx("div",{});export{s as default};
