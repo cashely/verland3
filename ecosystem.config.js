f033659f7e90ea193c66cedaff7927765e4f7f3f
cashely <290119516@qq.com>
2025-05-12 22:27:27 +0800

    fix: 修改正式环境配置 #dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'pnpm api:prod', // <-- 这里设置为你的 package.json 中的启动脚本命令 (例如 'pnpm start' 或 'pnpm serve')
   
       // 以下是一些常用的可选配置：
-      instances: 'max', // 应用实例数量，如果是 Node.js 应用，通常可以设置为 'max' 来利用多核
+      instances: 1, // 应用实例数量，如果是 Node.js 应用，通常可以设置为 'max' 来利用多核
       autorestart: true, // 应用崩溃时自动重启
       watch: false, // 是否监听文件变化并重启 (生产环境通常设置为 false)
       max_memory_restart: '3G', // 内存占用超过 1GB 时重启
